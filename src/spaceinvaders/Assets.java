ceb298c9018258461a92b2e5d91bf6176c42b56b
gggfox <gerardogalangarzafox@gmail.com>
2019-03-08 08:51:54 -0600

    los alien caen y estan animados

--- src/spaceinvaders/Assets.java
+++ src/spaceinvaders/Assets.java
@@ -15,9 +15,6 @@ public class Assets {
     
     public static BufferedImage background; // to store background image 
     public static BufferedImage player; // to store the player image
-    public static BufferedImage playerGrow; // to store the player image
-    public static BufferedImage grow; //imagen del poder
-    public static BufferedImage lives; //las imagenes de vidas
     public static BufferedImage pause; //variable de pausa
     public static BufferedImage gameover; //variable de que perdio
     public static BufferedImage win; //variable de que gano 
@@ -26,37 +23,39 @@ public class Assets {
     public static BufferedImage BallImages[]; 
     public static BufferedImage ballSprites; // to store the sprites 
     
-    public static BufferedImage BrickImages[];     // pictures to go down
-    public static BufferedImage brickSprites; // to store the sprites 
+    
+    public static BufferedImage AlienImages[];
+    public static BufferedImage alienSprites;
+    public static BufferedImage BombImages[];
 /**
 * initializing the images of the game */
 public static void init() {
-background = ImageLoader.loadImage("/Images/desert.jpeg"); //fondo del juego
-player = ImageLoader.loadImage("/Images/Minivan.png"); //la van
-playerGrow = ImageLoader.loadImage("/Images/MegaVan.png"); //la van grande
-pause = ImageLoader.loadImage("/Images/pause.png"); //imagen que indica pausa
-gameover = ImageLoader.loadImage("/Images/GameO.jpg"); //imagen de gameover
-win = ImageLoader.loadImage("/Images/win.png");//imagen de Win
-grow = ImageLoader.loadImage("/Images/GreenPower.png");//frasco de poder verde
-lives = ImageLoader.loadImage("/Images/Heisenberg.png"); //imagen de las vidas que es la cara de hank
-song = new SoundClip("/sounds/Megalovania.wav", -3f,true); //cancion del juego
-brickSprites = ImageLoader.loadImage("/Images/Meth.png"); //imagenes de bricks que son meths
-SpriteSheet spritesheet = new SpriteSheet(brickSprites); //sprite de bricks
-ballSprites = ImageLoader.loadImage("/Images/SpriteBall.png");//imagenes de ball
-SpriteSheet ballspritesheet = new SpriteSheet(ballSprites);//sprite de ball
+    background = ImageLoader.loadImage("/Images/desert.jpeg"); //fondo del juego
+    pause = ImageLoader.loadImage("/Images/pause.png"); //imagen que indica pausa
+    gameover = ImageLoader.loadImage("/Images/GameO.jpg"); //imagen de gameover
+    win = ImageLoader.loadImage("/Images/win.png");//imagen de Win
+    song = new SoundClip("/sounds/Megalovania.wav", -3f,true); //cancion del juego
+    ballSprites = ImageLoader.loadImage("/Images/SpriteBall.png");//imagenes de ball
+    SpriteSheet ballspritesheet = new SpriteSheet(ballSprites);//sprite de ball
 
-//se corta la imagen de los sprites de los bricks
-BrickImages = new BufferedImage[5];       
-BrickImages[0] = spritesheet.crop(0,0,256,150);
-BrickImages[1] = spritesheet.crop(256,0,256,150);
-BrickImages[3] = spritesheet.crop(0,155,256,150);
-BrickImages[2] = spritesheet.crop(256,155,256,150);
+    alienSprites = ImageLoader.loadImage("/Images/SpaceInvaders.png");
+    SpriteSheet alienSpriteSheet = new SpriteSheet(alienSprites);
 
-//se corta la imagen de los sprites de ball
-BallImages = new BufferedImage[8];
-for (int i = 0; i < 8; i++){
-    BallImages[i] = ballspritesheet.crop(0+40*i,0,32,32);
-}
+    AlienImages = new BufferedImage[14];
+    for(int k = 0; k < 14; k++){
+        AlienImages[k] = alienSpriteSheet.crop(0+16*k,17,15,15);
+    }
+    //se corta la imagen de los sprites de los bricks
+
+    player = alienSpriteSheet.crop(0,32,17,18);
+    //se corta la imagen de los sprites de ball
+    BallImages = new BufferedImage[8];
+    for (int i = 0; i < 8; i++){
+        BallImages[i] = ballspritesheet.crop(0+40*i,0,32,32);
+    }
+    BombImages = new BufferedImage[2];
+    BombImages[0] = alienSpriteSheet.crop(48,0,8,16);
+    BombImages[1] = alienSpriteSheet.crop(56,0,8,16);
 
 }
 
---
+++ src/spaceinvaders/Bomb.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spaceinvaders;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+/**
+ *
+ * @author Electel
+ */
+public class Bomb extends Item{
+
+    private int width;
+    private int height;
+    private Game game;
+    private int speed;
+
+    private Animation bomba; //la animacion de la pelota (esta girando)
+    private boolean bVisible; //
+    
+    public Bomb(int x, int y, int width, int height, Game game) {
+        super(x, y);
+        this.width = width;
+        this.height = height;
+        this.game = game;
+        this.speed = 2;
+        this.bomba = new Animation(Assets.BombImages, 100); //se inicializa con la imagen principal 
+        this.bVisible = true;
+    }
+    public boolean isVisible(){
+        return bVisible;
+    }
+    public void changeVisibility(boolean b){
+        this.bVisible = b;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    
+
+    
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+    
+
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public void tick() {
+        this.bomba.tick();
+        
+        if(game.isStart()){
+            if (game.isPausa()==false){
+        
+                setY(getY()+getSpeed());
+       
+            }
+    }
+    }
+    
+    public boolean intersecta(Player obj){
+     return obj instanceof Player  && getPerimetro().intersects(((Player) obj).getPerimetro());
+     }
+           
+    public Rectangle getPerimetro() {
+     return new Rectangle(getX(), getY(), getWidth(), getHeight());
+    }
+            
+    @Override
+    public void render(Graphics g) {
+        if(isVisible()){
+          g.drawImage(bomba.getCurrentFrame(), getX(), getY(), getWidth(), getHeight(), null);  
+        }
+        
+    }
+}
+
